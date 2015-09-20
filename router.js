13bab0f55df85d15ced2ae396caae6b0483456d4
Aaron Norby <aaron.norby@gmail.com>
2015-09-20 11:33:35 -0700

    Add router using express.Router()

---
+++ router.js
@@ -0,0 +1,11 @@
+var router = require('express').Router();
+
+/* Routes: 
+* adding a show to the collection (post),
+* getting missed stories from all shows (get)
+* sign-in and sign-up
+*
+*/
+
+
+module.exports = router;
