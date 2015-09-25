bd728a1a1b72ca9db83876fe1ff5b305439cde00
Aaron Norby <aaron.norby@gmail.com>
2015-09-24 20:03:11 -0700

    Add date formatting and flexible days with momentjs
    Flexible dates means that we get stories from the last seven days relative to
    the date of the request. Date formatting means that we display the
    date in a more readable format.

--- utils.js
+++ utils.js
@@ -1,24 +1,25 @@
 var request = require('request');
 var Promise = require('bluebird');
-
+var moment = require('moment');
 
 var apiKey = process.env.NPRKEY;
+var today = moment().format('YYYY-M-D');
+var aWeekAgo = moment().subtract(7, 'days').format('YYYY-M-D');
 var uriFirstPart = 'http://api.npr.org/query?id=';
 //need to add flexible dates;
-var uriSecondPart = '&fields=teaser,storyDate,audio&startDate=2015-09-12&endDate=2015-09-19&dateType=story&output=JSON&apiKey=';
+var uriSecondPart = '&fields=teaser,storyDate,audio&startDate=';
+var uriThirdPart = '&endDate=';
+var uriFourthPart = '&dateType=story&output=JSON&numResults=50&apiKey=';
 
-
-// module.exports.getStories = function(showId) {
-//   var uri = uriFirstPart + showId + uriSecondPart + apiKey;
-//   return getAsync({uri: uri, json: true});
-// };
 
 
 module.exports.getStories = function(showId) {
   return new Promise(function(resolve, reject) {
-    var uri = uriFirstPart + showId + uriSecondPart + apiKey;
+    var uri = uriFirstPart + showId + uriSecondPart + 
+      aWeekAgo + uriThirdPart + today + uriFourthPart + apiKey;
+    console.log(uri);
+    
     request.get({uri: uri, json: true}, function(err, response, body) {
-      //console.log(body);
       if (err) reject(err);
       resolve(body);
     });
