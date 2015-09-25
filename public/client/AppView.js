1c32413183bfff4e6c28109c318c5fc8e5ffc3e6
Aaron Norby <aaron.norby@gmail.com>
2015-09-24 19:00:23 -0700

    Add ability to switch shows via click and data-showid attribute

--- public/client/AppView.js
+++ public/client/AppView.js
@@ -5,9 +5,30 @@ var AppView = Backbone.View.extend({
     this.showview = new ShowView({collection: this.model.get('show')});
   },
 
+  events: {
+    'click .menu-item': 'swap'
+  },
+
+
+  swap: function(e) {
+    console.log($(e.currentTarget).data('showid'));
+    var id = $(e.currentTarget).data('showid');
+    this.model.get('show').url = '/api/stories/' + id;
+    this.model.get('show').fetch();
+  },
+
   render: function() {
     this.$el.html(this.template());
     this.$el.find('#show').append(this.showview.render());
     return this.el;
   }
 });
+
+
+// 'click .atc': this.swap('atc'),
+//     'click .me': this.swap('me'),
+//     'click .fa': this.swap('fa'),
+//     'click .han': this.swap('han'),
+//     'click .lusa': this.swap('lusa'),
+//     'click .wesat': this.swap('wesat'),
+//     'click .wesun': this.swap('wesun')
